a985a41fbe1e68a3e716655ce8b8c0ce02e2c55b
Leonel Braginski <leobraginski@gmail.com>
2019-07-02 15:03:12 -0300

    15/03

--- Casi-evaluacion/app/src/main/java/com/ort/casievaluacion/MainActivity.java
+++ Casi-evaluacion/app/src/main/java/com/ort/casievaluacion/MainActivity.java
@@ -2,8 +2,10 @@ package com.ort.casievaluacion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
