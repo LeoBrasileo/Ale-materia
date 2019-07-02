2b07185167de0478c3d724c12ae227c829310dd1
Leonel Braginski <leobraginski@gmail.com>
2019-07-02 15:53:05 -0300

    safe commit

--- Casi-evaluacion/app/src/main/java/com/ort/casievaluacion/MainActivity.java
+++ Casi-evaluacion/app/src/main/java/com/ort/casievaluacion/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listview);
+        //safe commit
 
         ArrayList<Personas> personas = new ArrayList<Personas>();
 
