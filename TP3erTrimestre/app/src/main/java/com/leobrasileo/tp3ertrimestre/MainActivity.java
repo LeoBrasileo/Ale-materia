47a304b62ade7937cdcf6d8b9bd418863790b9bf
Leonel Braginski <leobraginski@gmail.com>
2019-09-24 14:15:37 -0300

    mas cosas

--- TP3erTrimestre/app/src/main/java/com/leobrasileo/tp3ertrimestre/MainActivity.java
+++ TP3erTrimestre/app/src/main/java/com/leobrasileo/tp3ertrimestre/MainActivity.java
@@ -1,11 +1,16 @@
 package com.leobrasileo.tp3ertrimestre;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Button bt1 = (Button) findViewById(R.id.button1);
         Button bt2 = (Button) findViewById(R.id.button2);
         Button bt3 = (Button) findViewById(R.id.button3);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
