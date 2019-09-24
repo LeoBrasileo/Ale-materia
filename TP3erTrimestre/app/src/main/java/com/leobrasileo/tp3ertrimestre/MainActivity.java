3059a898f5a1eb69e64ba53ac8a2cad6fd0d1c05
Leonel Braginski <leobraginski@gmail.com>
2019-09-24 14:10:13 -0300

    botones

--- TP3erTrimestre/app/src/main/java/com/leobrasileo/tp3ertrimestre/MainActivity.java
+++ TP3erTrimestre/app/src/main/java/com/leobrasileo/tp3ertrimestre/MainActivity.java
@@ -2,6 +2,7 @@ package com.leobrasileo.tp3ertrimestre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt1 = (Button) findViewById(R.id.button1);
+        Button bt2 = (Button) findViewById(R.id.button2);
+        Button bt3 = (Button) findViewById(R.id.button3);
     }
 }
